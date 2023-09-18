5c700a5a19f6865eeeb8ba108fb93e7ab4ab7db0
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-09-18 17:07:08 +0630

    local > noti

---
+++ lib/firebase_api.dart
@@ -0,0 +1,95 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:google_fcm/main.dart';
+import 'package:google_fcm/page/notification_screen.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  log('Title :  ${message.notification?.title}');
+  log('Body : ${message.notification?.body}');
+  log('Payload : ${message.data}');
+}
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.defaultImportance,
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  void handleMessage(RemoteMessage? message) {
+    log('handleMessage : ${message.toString()}');
+    if (message == null) return;
+
+    navigatorKey.currentState?.pushNamed(
+      NotificationScreen.route,
+      arguments: message,
+    );
+  }
+
+  Future<void> initLocalNotifications() async {
+    // const ios = notifications
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (details) {
+        final message = RemoteMessage.fromMap(jsonDecode(details.toString()));
+        handleMessage(message);
+      },
+      onDidReceiveBackgroundNotificationResponse: (details) {
+        final message = RemoteMessage.fromMap(jsonDecode(details.toString()));
+        handleMessage(message);
+      },
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
+  }
+
+  Future<void> initPushNotifications() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+      _localNotifications.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              _androidChannel.id,
+              _androidChannel.name,
+              channelDescription: _androidChannel.description,
+              icon: '@drawable/ic_launcher',
+            ),
+          ),
+          payload: jsonEncode(message.toMap()));
+    });
+  }
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fcmToken = await _firebaseMessaging.getToken();
+    log('fcmToken : $fcmToken');
+    initPushNotifications();
+    initLocalNotifications();
+  }
+}
